55d3defab009bb62118a54a4991b0ec9d6666c4b
VikramKes <vikram.keswani@students.iiit.ac.in>
2020-10-31 21:01:16 +0530

    second_commit

---
+++ Wallet_Amount/src/main/java/com/example/Wallet_Amount/WalletAmountApplication.java
@@ -0,0 +1,13 @@
+package com.example.Wallet_Amount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletAmountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletAmountApplication.class, args);
+	}
+
+}
---
+++ Wallet_Amount/src/test/java/com/example/Wallet_Amount/WalletAmountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Wallet_Amount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletAmountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
